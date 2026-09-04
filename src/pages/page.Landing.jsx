c248470e5ec5364bc972da20d65a2764193c4f35
Alpha <shankhodeepdas609@gmail.com>
2026-09-04 21:59:28 +0530

    added landing page

--- src/pages/page.Landing.jsx
+++ src/pages/page.Landing.jsx
@@ -3,15 +3,18 @@ import { useNavigate } from "react-router-dom"
 export default function LandingPage(){
     const navigate = useNavigate();
     const handleSignup = () => {navigate("/signup")};
-    const handleAi =() => {navigate("/dashboard/ai")};
+    const handleDashboard =() => {navigate("/dashboard")};
     return(
         <div className="landing-page">
-            <button onClick={handleSignup}>
-                Sign Up
-            </button>
-            <button onClick={handleAi}>
-                Ai
-            </button>
+            <div className="cta-container">
+                <button className="btn-primary-sign-up" onClick={handleSignup}>
+                    Sign Up ➡
+                </button>
+
+                <button className="btn-secondary-ai-launcher" onClick={handleDashboard}>
+                    Dashboard
+                </button>
+            </div>
         </div>
     );
 }
